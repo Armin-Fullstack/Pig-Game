427f99a945079c947ee4f5e181212df6b3cca9c4
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-04-28 20:12:50 +0330

    Feat: Hide dice

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,4 @@
 "use strict";
+// selecting elements
+const dice = document.querySelector(".dice");
+dice.classList.add("hidden");
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+// selecting elements
+const dice = document.querySelector(".dice") ! as HTMLImageElement
+
+dice.classList.add("hidden")
