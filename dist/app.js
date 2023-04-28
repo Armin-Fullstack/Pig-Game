90034e6a2fb6f2813f8d32545e0583d61b73de23
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-04-28 20:33:12 +0330

    Feat: Generate randome dice

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,17 @@
 "use strict";
 // selecting elements
-const dice = document.querySelector(".dice");
-dice.classList.add("hidden");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+// starting conditions
+diceEl.classList.add("hidden");
+// roll dice functionality 
+const rollDice = () => {
+    // 1. Generate randome dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // 2. display dice roll
+    console.log(dice);
+    // 3. if it is 1 then change the player else add dice roll
+};
+btnRoll.addEventListener("click", rollDice);
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,19 @@
 // selecting elements
-const dice = document.querySelector(".dice") ! as HTMLImageElement
+const diceEl = document.querySelector(".dice") ! as HTMLImageElement
+const btnNew = document.querySelector(".btn--new") ! as HTMLButtonElement
+const btnRoll = document.querySelector(".btn--roll") ! as HTMLButtonElement
+const btnHold = document.querySelector(".btn--hold") ! as HTMLButtonElement
 
-dice.classList.add("hidden")
+// starting conditions
+diceEl.classList.add("hidden")
+
+// roll dice functionality 
+const rollDice = ()  => {
+  // 1. Generate randome dice roll
+  const dice = Math.trunc(Math.random() * 6 ) + 1
+  // 2. display dice roll
+  // 3. if it is 1 then change the player else add dice roll
+}
+
+
+btnRoll.addEventListener("click" , rollDice)
