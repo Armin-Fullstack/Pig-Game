b43c880ddabe2033cf34c92bf724dbaf3a6151a3
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-04-29 20:48:43 +0330

    Feat: Added playing status

--- dist/app.js
+++ dist/app.js
@@ -11,6 +11,7 @@ diceEl.classList.add("hidden");
 let currentScore = 0;
 let totalScore = [0, 0];
 let activePlayer = 0;
+let playing = true;
 const switchPlayer = () => {
     document.getElementById(`current--${activePlayer}`).textContent = String(0);
     currentScore = 0;
@@ -20,33 +21,39 @@ const switchPlayer = () => {
 };
 // roll dice functionality
 const rollDice = () => {
-    // 1. Generate randome dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    // 2. display dice roll
-    diceEl.classList.remove("hidden");
-    diceEl.src = `Images/dice-${dice}.png`;
-    // 3. if it is 1 then change the player else add dice roll
-    if (dice !== 1) {
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = String(currentScore);
-    }
-    else {
-        // change the active player
-        switchPlayer();
+    if (playing) {
+        // 1. Generate randome dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
+        // 2. display dice roll
+        diceEl.classList.remove("hidden");
+        diceEl.src = `Images/dice-${dice}.png`;
+        // 3. if it is 1 then change the player else add dice roll
+        if (dice !== 1) {
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = String(currentScore);
+        }
+        else {
+            // change the active player
+            switchPlayer();
+        }
     }
 };
 const holdScore = () => {
-    // 1. add current score to the active player's score
-    totalScore[activePlayer] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent = String(totalScore[activePlayer]);
-    // 2. if >= 100 finish game
-    if (totalScore[activePlayer] >= 20) {
-        document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
-        document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");
-    }
-    else {
-        // 3. change the active player
-        switchPlayer();
+    if (playing) {
+        // 1. add current score to the active player's score
+        totalScore[activePlayer] += currentScore;
+        document.getElementById(`score--${activePlayer}`).textContent = String(totalScore[activePlayer]);
+        // 2. if >= 100 finish game
+        if (totalScore[activePlayer] >= 20) {
+            playing = false;
+            diceEl.classList.add("hidden");
+            document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
+            document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");
+        }
+        else {
+            // 3. change the active player
+            switchPlayer();
+        }
     }
 };
 btnRoll.addEventListener("click", rollDice);
--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,7 @@ diceEl.classList.add("hidden");
 let currentScore = 0;
 let totalScore = [0, 0];
 let activePlayer = 0;
+let playing = true;
 
 const switchPlayer = (): void => {
   (
@@ -28,45 +29,51 @@ const switchPlayer = (): void => {
 
 // roll dice functionality
 const rollDice = (): void => {
-  // 1. Generate randome dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  // 2. display dice roll
-  diceEl.classList.remove("hidden");
-  diceEl.src = `Images/dice-${dice}.png`;
-  // 3. if it is 1 then change the player else add dice roll
-  if (dice !== 1) {
-    currentScore += dice;
-    (
-      document.getElementById(
-        `current--${activePlayer}`
-      )! as HTMLParagraphElement
-    ).textContent = String(currentScore);
-  } else {
-    // change the active player
-    switchPlayer();
+  if (playing) {
+    // 1. Generate randome dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // 2. display dice roll
+    diceEl.classList.remove("hidden");
+    diceEl.src = `Images/dice-${dice}.png`;
+    // 3. if it is 1 then change the player else add dice roll
+    if (dice !== 1) {
+      currentScore += dice;
+      (
+        document.getElementById(
+          `current--${activePlayer}`
+        )! as HTMLParagraphElement
+      ).textContent = String(currentScore);
+    } else {
+      // change the active player
+      switchPlayer();
+    }
   }
 };
 const holdScore = (): void => {
-  // 1. add current score to the active player's score
-  totalScore[activePlayer] += currentScore;
-  (
-    document.getElementById(`score--${activePlayer}`)! as HTMLParagraphElement
-  ).textContent = String(totalScore[activePlayer]);
-  // 2. if >= 100 finish game
-  if (totalScore[activePlayer] >= 20) {
+  if (playing) {
+    // 1. add current score to the active player's score
+    totalScore[activePlayer] += currentScore;
     (
-      document.querySelector(
-        `.player--${activePlayer}`
-      )! as HTMLTableSectionElement
-    ).classList.add("player--winner");
-    (
-      document.querySelector(
-        `.player--${activePlayer}`
-      )! as HTMLTableSectionElement
-    ).classList.remove("player--active");
-  } else {
-    // 3. change the active player
-    switchPlayer();
+      document.getElementById(`score--${activePlayer}`)! as HTMLParagraphElement
+    ).textContent = String(totalScore[activePlayer]);
+    // 2. if >= 100 finish game
+    if (totalScore[activePlayer] >= 20) {
+      playing = false;
+    diceEl.classList.add("hidden");
+      (
+        document.querySelector(
+          `.player--${activePlayer}`
+        )! as HTMLTableSectionElement
+      ).classList.add("player--winner");
+      (
+        document.querySelector(
+          `.player--${activePlayer}`
+        )! as HTMLTableSectionElement
+      ).classList.remove("player--active");
+    } else {
+      // 3. change the active player
+      switchPlayer();
+    }
   }
 };
 
