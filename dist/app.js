d4528e9555012c4f16190a2d42887b499a963549
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-04-29 18:57:28 +0330

    Feat: Change the player

--- dist/app.js
+++ dist/app.js
@@ -4,10 +4,12 @@ const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
-const currentScoreElPlayer0 = document.querySelector("#current--0");
+const player0 = document.querySelector(".player--0");
+const player1 = document.querySelector(".player--1");
 // starting conditions
 diceEl.classList.add("hidden");
 let currentScore = 0;
+let activePlayer = 0;
 // roll dice functionality 
 const rollDice = () => {
     // 1. Generate randome dice roll
@@ -18,10 +20,15 @@ const rollDice = () => {
     // 3. if it is 1 then change the player else add dice roll
     if (dice !== 1) {
         currentScore += dice;
-        currentScoreElPlayer0.textContent = String(currentScore);
+        document.getElementById(`current--${activePlayer}`).textContent = String(currentScore);
     }
     else {
         // change the player
+        document.getElementById(`current--${activePlayer}`).textContent = String(0);
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0.classList.toggle("player--active");
+        player1.classList.toggle("player--active");
     }
 };
 btnRoll.addEventListener("click", rollDice);
--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,13 @@ const diceEl = document.querySelector(".dice") ! as HTMLImageElement
 const btnNew = document.querySelector(".btn--new") ! as HTMLButtonElement
 const btnRoll = document.querySelector(".btn--roll") ! as HTMLButtonElement
 const btnHold = document.querySelector(".btn--hold") ! as HTMLButtonElement
-const currentScoreElPlayer0 = document.querySelector("#current--0") ! as HTMLParagraphElement
+const player0 = document.querySelector(".player--0") ! as HTMLTableSectionElement
+const player1 = document.querySelector(".player--1") ! as HTMLTableSectionElement
 
 // starting conditions
 diceEl.classList.add("hidden")
 let currentScore = 0;
+let activePlayer = 0;
 // roll dice functionality 
 const rollDice = ()  => {
   // 1. Generate randome dice roll
@@ -19,9 +21,14 @@ const rollDice = ()  => {
   // 3. if it is 1 then change the player else add dice roll
   if(dice !== 1) {
     currentScore += dice;
-    currentScoreElPlayer0.textContent = String(currentScore)
+    (document.getElementById(`current--${activePlayer}`) ! as HTMLParagraphElement).textContent = String(currentScore)
   } else {
     // change the player
+    (document.getElementById(`current--${activePlayer}`) ! as HTMLParagraphElement).textContent = String(0)
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle("player--active")
+    player1.classList.toggle("player--active")
   }
 }
 
